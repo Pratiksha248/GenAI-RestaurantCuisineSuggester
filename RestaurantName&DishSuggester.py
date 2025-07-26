03c0cc8d644cb0abd8431ace3421d8b056abb4a3
Pratiksha Sahoo <sahoopratiksha24@gmail.com>
2025-07-27 02:05:25 +0530

    Add files via upload

---
+++ RestaurantName&DishSuggester.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[28]:
+
+
+import os
+os.environ['GOOGLE_API_KEY'] = 'your-google-ai-api-key'
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+
+llm = ChatGoogleGenerativeAI(model="models/gemini-2.5-pro", temperature=0.9)
+
+prompt = PromptTemplate.from_template("I want to open an Indian restrobar, suggest me a fancy name for it and a {cuisine} dish.")
+# response = llm.invoke(prompt)
+
+chain = prompt | llm
+
+# Run the chain
+response = chain.invoke({"cuisine": "Chinese"})
+
+# Print the result
+print(response.content)
+
+
+# In[23]:
+
+
+import google.generativeai as genai
+
+genai.configure(api_key="your-google-ai-api-key")
+
+models = genai.list_models()
+
+for model in models:
+    print(model.name)
+
+
+# In[ ]:
+
+
+
+
